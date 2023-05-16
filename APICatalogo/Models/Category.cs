d539322babce3fb22a80cfd0af059201d83fc010
Gustavo F. Balbino <gustavofbalbino@icloud.com>
2023-05-16 16:01:00 -0300

    added database settings

--- APICatalogo/Models/Category.cs
+++ APICatalogo/Models/Category.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace APICatalogo.Models;
 
+//instead of data anotations, fluent API can be used to avoid pollution in our
+//model/entity class
+
 public class Category {
-	public int CategoryId { get; set; }
-	public string? Name { get; set; }
-	public string? ImageUrl { get; set; }
+
+    public Category() {
+        Products = new Collection<Product>();
+    }
+
+    [Key]
+    public int CategoryId { get; set; }
+    [Required]
+    [StringLength(80)]
+    public string? Name { get; set; }
+    [Required]
+    [StringLength(300)]
+    public string? ImageUrl { get; set; }
+
+    public ICollection<Product>? Products { get; set; }
 }
 
--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -7,11 +10,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
